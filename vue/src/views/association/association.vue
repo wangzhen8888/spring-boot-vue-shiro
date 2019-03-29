97af6d9f6584a84e4166732227327fd907ae4940
wangzhen8888 <691002936@qq.com>
2019-03-29 16:04:25 +0800

    更新： 完善前端vue上传组件第一版（和接口调试正常）

--- vue/src/views/association/association.vue
+++ vue/src/views/association/association.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="association">
     <div class="filter-container">
+  <el-upload
+ class="upload-demo"
+ ref="upload"
+ :action="doUpload()"
+ :limit="1"
+ :file-list="fileList"
+ :before-upload="beforeUpload">
+ <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+ <a href="./static/moban.xlsx" rel="external nofollow" download="模板"><el-button size="small" type="success">下载模板</el-button></a>
+ 
+ <div slot="tip" class="el-upload__tip">只能上传excel文件，且不超过5MB</div>
+ <div slot="tip" class="el-upload-list__item-name">{{fileName}}</div>
+</el-upload> 
+<span slot="footer" class="dialog-footer">
+ <el-button @click="visible = false">取消</el-button>
+ <el-button type="primary" @click="submitUpload()">确定</el-button>
+</span>
       <el-form>
         <el-form-item>
           <el-button
@@ -146,8 +163,8 @@
     :data="userList"
     style="width: 100%">
     <el-table-column
-      label="ID"
-      prop="userId">
+      label="学号"
+      prop="stuNum">
     </el-table-column>
     <el-table-column
       label="Name"
@@ -177,6 +194,9 @@
 export default {
   data() {
     return {
+      
+      fileList: [],
+      fileName:"",
       totalCount: 0, //分页组件--数据总条数
       list: [], //表格的数据
       listLoading: false, //数据加载等待动画
@@ -215,6 +235,76 @@ export default {
     this.getList();
   },
   methods: {
+    doUpload(){
+return "http:cccc.com"
+    },
+    beforeUpload(file){
+  // debugger
+  console.log(file,'文件');
+  this.files = file;
+  const extension = file.name.split('.')[1] === 'xls'
+  const extension2 = file.name.split('.')[1] === 'xlsx'
+  const isLt2M = file.size / 1024 / 1024 < 5
+  if (!extension && !extension2) {
+   this.$message.warning('上传模板只能是 xls、xlsx格式!')
+   return
+  }
+  if (!isLt2M) {
+   this.$message.warning('上传模板大小不能超过 5MB!')
+   return
+  }
+  this.fileName = file.name;
+  return false // 返回false不会自动上传
+  },
+  submitUpload() {
+  // debugger
+  console.log('上传'+this.files.name)
+  if(this.fileName == ""){
+   this.$message.warning('请选择要上传的文件！')
+   return false
+  }
+  let file = new FormData();
+  file.append('file', this.files, this.fileName);//filename是键，file是值，就是要传的文件，test.zip是要传的文件名
+  let requestConfig = {
+   headers: {
+   'Content-Type': 'multipart/form-data'
+   },
+  }
+  // this.api({
+  //       url: "/resolve/upload",
+  //       method: "post",
+  //       params: file,
+  //       headers: {
+  //  'Content-Type': 'multipart/form-data'
+  //  }
+  //     }).then(data => {
+  //       console.log(data);
+  //       this.listLoading = false;
+       
+  //     });
+   this.api.post(`/resolve/upload`, file, requestConfig).then((res) => {
+  //  debugger
+   if (data && data.code === 0) {
+   this.$message({
+    message: '操作成功',
+    type: 'success',
+    duration: 1500,
+    onClose: () => {
+    this.visible = false
+    this.$emit('refreshDataList')
+    }
+   })
+   } else {
+   this.$message.error(data.msg)
+   }
+  }) 
+  },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
     handleEdit(index, row) {
         console.log(index, row);
       },
